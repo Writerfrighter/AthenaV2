fe1b12879e607fba51fb4c40163ff484a635781a
Writerfrighter <109774580+Writerfrighter@users.noreply.github.com>
2025-10-08 18:20:41 -0700

    Refactor icons to assets

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -26,44 +26,44 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: 'Match Scout',
         description: 'Start match scouting',
         url: '/scout/matchscout',
-        icons: [{ src: '/icons/icon-192.png', sizes: '192x192' }]
+        icons: [{ src: '/assets/icon-192.png', sizes: '192x192' }]
       },
       {
         name: 'Pit Scouting',
         short_name: 'Pit Scout',
         description: 'Start pit scouting',
         url: '/scout/pitscout',
-        icons: [{ src: '/icons/icon-192.png', sizes: '192x192' }]
+        icons: [{ src: '/assets/icon-192.png', sizes: '192x192' }]
       },
       {
         name: 'Dashboard',
         short_name: 'Dashboard',
         description: 'View scouting dashboard',
         url: '/dashboard',
-        icons: [{ src: '/icons/icon-192.png', sizes: '192x192' }]
+        icons: [{ src: '/assets/icon-192.png', sizes: '192x192' }]
       }
     ],
     icons: [
       {
-        src: '/icons/icon-192.png',
+        src: '/assets/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/icons/icon-512.png',
+        src: '/assets/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/icons/icon-512.png',
+        src: '/assets/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/icons/icon-192.png',
+        src: '/assets/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable'
